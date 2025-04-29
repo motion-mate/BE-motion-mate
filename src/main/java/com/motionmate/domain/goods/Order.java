bde59d280a11b00866baeff8a0e89836f65118ff
aaa <hoonrara2@gmail.com>
2025-04-29 14:47:37 +0900

    refactor: User, Order 엔티티 테이블명을 복수형으로 변경 (users, orders)

--- src/main/java/com/motionmate/domain/goods/Order.java
+++ src/main/java/com/motionmate/domain/goods/Order.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "orders")
 public class Order {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
