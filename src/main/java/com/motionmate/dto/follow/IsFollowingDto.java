1ad8420f677a60bb1e4a61b25040221c2b90287b
kokeoung <ko908599@gmail.com>
2025-05-01 11:09:07 +0900

    fix[팔로우/팔로잉 목록불러오기 500서버에러 수정완료]

--- src/main/java/com/motionmate/dto/follow/IsFollowingDto.java
+++ src/main/java/com/motionmate/dto/follow/IsFollowingDto.java
@@ -1,6 +1,14 @@
 package com.motionmate.dto.follow;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+@Builder
 public class IsFollowingDto {
 
+    //  프론트 작업 시 true -> 팔로우버튼, false -> 팔로잉버튼
     private boolean isFollowing;
 }
---
+++ src/main/java/com/motionmate/mapper/FollowMapper.java
@@ -0,0 +1,37 @@
+package com.motionmate.mapper;
+
+import com.motionmate.domain.follow.Follow;
+import com.motionmate.domain.user.User;
+import com.motionmate.dto.follow.FollowRequestDto;
+import com.motionmate.dto.follow.FollowResponseDto;
+import com.motionmate.dto.follow.IsFollowingDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FollowMapper {
+
+    // Follow -> FollowResponseDto
+    public static FollowResponseDto toDto(User user) {
+        return FollowResponseDto.builder()
+                .userId(user.getId())
+                .nickname(user.getNickname())
+                .profileImageUrl(user.getProfileImageUrl())
+                .build();
+    }
+
+    // FollowRequestDto -> Follow
+    public static Follow toEntity(FollowRequestDto dto, User fromUser, User toUser) {
+        return Follow.builder()
+                .fromUser(fromUser)
+                .toUser(toUser)
+                .build();
+    }
+
+    public static IsFollowingDto toIsFollowingDto(boolean isFollowing) {
+        return IsFollowingDto.builder()
+                .isFollowing(isFollowing)
+                .build();
+    }
+
+
+}
