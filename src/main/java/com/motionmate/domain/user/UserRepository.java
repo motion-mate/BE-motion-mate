f241dc3e78c69ef9e3af3b95a8212840e12d6b0e
mmg0223 <mmg0223@gmail.com>
2025-05-01 17:33:49 +0900

    유저 레포지토리 수정

--- src/main/java/com/motionmate/domain/user/UserRepository.java
+++ src/main/java/com/motionmate/domain/user/UserRepository.java
@@ -1,9 +1,14 @@
 package com.motionmate.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.profile.nickname = :nickname")
+    Optional<User> findUserByNickname(@Param("nickname") String nickname);
 }
