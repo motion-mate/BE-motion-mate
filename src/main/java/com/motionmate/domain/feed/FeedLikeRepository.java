10c74e0ef0512d018f998fd6987c86f60bfe5212
kyj <jjjin1215@naver.com>
2025-05-02 10:31:28 +0900

    좋아요 기능

--- src/main/java/com/motionmate/domain/feed/FeedLikeRepository.java
+++ src/main/java/com/motionmate/domain/feed/FeedLikeRepository.java
@@ -3,6 +3,8 @@ package com.motionmate.domain.feed;
 import com.motionmate.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FeedLikeRepository extends JpaRepository<FeedLike, Long> {
 
     //내가 좋아요 눌렀는지 여부
@@ -10,4 +12,6 @@ public interface FeedLikeRepository extends JpaRepository<FeedLike, Long> {
 
     //해당 피드에 눌린 좋아요 수
     int countByFeed(Feed feed);
+
+    Optional<FeedLike> findByFeedAndUser(Feed feed, User user);
 }
