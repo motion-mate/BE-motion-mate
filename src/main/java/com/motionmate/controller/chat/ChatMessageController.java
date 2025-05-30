e63fbff73ab46efb5f05a2b7fa33132840238376
mmg0223 <mmg0223@gmail.com>
2025-05-30 12:40:37 +0900

    채팅방 리팩토링

--- src/main/java/com/motionmate/controller/chat/ChatMessageController.java
+++ src/main/java/com/motionmate/controller/chat/ChatMessageController.java
@@ -43,32 +43,6 @@ public class ChatMessageController {
 
         dto.setChatRoomId(roomId);
         dto.setSenderNickname(user.getProfile().getNickname());
-        /*
-        if (dto.getType() == ChatMessage.MessageType.ENTER) {
-            // 입장 메시지만 즉시 저장 (선택사항)
-            ChatRoom room = chatRoomRepository.findById(roomId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 채팅방입니다."));
-
-            /*
-            Optional<ChatRoomParticipant> participantOpt =
-                    chatRoomParticipantRepository.findByChatRoom_idAndUser(roomId, user);
-
-            if (participantOpt.isEmpty()) {
-                log.warn("❗ ChatRoomParticipant 조회 실패: user={}, room={}", user.getId(), roomId);
-            } else {
-                ChatRoomParticipant participant = participantOpt.get();
-                log.info("✅ ChatRoomParticipant 조회 성공: user={}, connected={}, room={}",
-                        user.getId(), participant.isConnected(), roomId);
-
-                participant.reconnect(); // connected = true
-                chatRoomParticipantRepository.save(participant);
-            }
-
-
-
-            // chatMessageService.saveEnterMessage(room, user);
-        }
-        //*/
-
 
         // TALK 등 일반 메시지는 저장하지 않고 Redis로만 전송
         log.info("📤 RedisPublisher.publish() 호출됨: {}", dto);
