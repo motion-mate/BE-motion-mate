857e25931a3cda8161f9ab8d769ab4720a64a321
mmg0223 <mmg0223@gmail.com>
2025-06-04 16:01:01 +0900

    Param 어노테이션 변경

--- src/main/java/com/motionmate/domain/chat/ChatRoomParticipantRepository.java
+++ src/main/java/com/motionmate/domain/chat/ChatRoomParticipantRepository.java
@@ -1,7 +1,7 @@
 package com.motionmate.domain.chat;
 
 import com.motionmate.domain.user.User;
-import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.repository.query.Param;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
