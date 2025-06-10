8e785e0a032f634f02ca2ea99cef2bba89f72909
mmg0223 <mmg0223@gmail.com>
2025-06-10 17:26:42 +0900

    cors ì설정 수정

--- src/main/java/com/motionmate/global/config/WebConfig.java
+++ src/main/java/com/motionmate/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 요청에 대해
-                .allowedOrigins("http://localhost:3000") // 프론트 주소
+                .allowedOrigins("http://52.79.118.166:3000") // 프론트 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization") // 토큰 헤더 클라이언트에서 읽기 가능하게
