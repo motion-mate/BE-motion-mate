23be127b1b75ee9a794ceab8b967758e9c405893
kyj <jjjin1215@naver.com>
2025-06-10 10:16:16 +0900

    feat:피드 최적화 코드 수정

--- src/main/java/com/motionmate/domain/feed/FeedLikeRepository.java
+++ src/main/java/com/motionmate/domain/feed/FeedLikeRepository.java
@@ -3,7 +3,9 @@ package com.motionmate.domain.feed;
 import com.motionmate.domain.user.User;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Objects;
@@ -24,7 +26,13 @@ public interface FeedLikeRepository extends JpaRepository<FeedLike, Long> {
     @Query("SELECT f.feed.id, COUNT(f) FROM FeedLike f WHERE f.feed.id IN :feedIds GROUP BY f.feed.id")
     List<Object[]> countLikesByFeedIds(@Param("feedIds") List<Long> feedIds);
 
+    // 내가 누른 피드 ID 목록
     @Query("SELECT f.feed.id FROM FeedLike f WHERE f.user.id = :userId")
     List<Long> findLikedFeedIdByUserId(@Param("userId") Long userId);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM FeedLike fl WHERE fl.feed.id = :feedId")
+    void deleteByFeedId(@Param("feedId") Long feedId);
 
 }
